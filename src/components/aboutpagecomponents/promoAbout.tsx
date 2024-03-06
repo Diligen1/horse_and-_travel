f244b0d637cf4a64bb955d834fb7be933e8fae16
Diligen1 <kuba.2000.0505.kg@gmail.com>
2024-03-06 17:10:55 +0600

    17:11

--- src/components/aboutpagecomponents/promoAbout.tsx
+++ src/components/aboutpagecomponents/promoAbout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Header } from "@/components";
 import Image from "next/image";
-import image from "@/assets/images/promoHome/banner.jpeg";
+import { useState, useEffect } from "react";
 import { Comfortaa } from "next/font/google";
 const comfortaa = Comfortaa({
   weight: ["400"],
@@ -10,30 +10,47 @@ const comfortaa = Comfortaa({
 });
 
 export default function PromoAbout() {
+  const [imageData, setImageData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://horse-travel.com/baner/detail/baner/1/?Accept=application/json",
+          {
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+        const responseData = await response.json();
+        setImageData(responseData[0].image);
+      } catch (error) {
+        console.error("Ошибка при загрузке данных:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className="mx-auto">
       <Header />
       <div className="w-full relative  flex justify-center items-start">
-        <Image
-          src={image}
-          className="w-full lg:max-h-[760px] md:max-h-[560px] max-h-[360px]"
-          alt="#"
-          priority
-        />
+        {imageData && (
+          <Image
+            src={imageData}
+            width={1080}
+            height={1000}
+            className="w-full lg:max-h-[760px] md:max-h-[560px] max-h-[360px] object-cover"
+            alt="#"
+          />
+        )}
         <div className="absolute top-[50%] left-[50%] z-[5] translate-x-[-50%] translate-y-[-50%]">
           <h1
             className={`sm:text-[36px] text-white  text-[18px] cursor-default text-center ${comfortaa.className}`}
           >
             About us
           </h1>
-          <h3
-            className={`text-[18px] text-white text-center ${comfortaa.className}`}
-          >
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-            nam quia. Aspernatur quia perferendis, nesciunt cum unde doloribus
-            laborum. Beatae adipisci quia ullam repellendus quod quidem
-            reprehenderit explicabo nostrum voluptate!
-          </h3>
         </div>
       </div>
     </main>
