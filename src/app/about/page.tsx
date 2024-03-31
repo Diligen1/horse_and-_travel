dd6a98cb80ae04fa547d937a66df4e3837b8265f
beksultan <hfjcgyrishwidhzhd1038@gmail.com>
2024-03-31 21:23:23 +0600

    seo update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,16 @@
-import { FooterSection } from "@/components";
-import { PromoAbout } from "@/components";
-import { FeedbackAbout } from "@/components";
-export default function AboutUs() {
-  return (
-    <main>
-      <PromoAbout />
-      <FeedbackAbout />
-      <FooterSection />
-    </main>
-  );
+import type { Metadata } from 'next'
+import { FeedbackAbout, FooterSection, PromoAbout } from '@/components'
+
+export const metadata: Metadata = {
+	title: 'About',
+}
+
+export default function Page() {
+	return (
+		<main>
+			<PromoAbout />
+			<FeedbackAbout />
+			<FooterSection />
+		</main>
+	)
 }
